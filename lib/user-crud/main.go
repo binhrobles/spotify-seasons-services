19006d0d078646b6374568f6cfdbcc40816066dc
Binh 🌱 <binhrobles@gmail.com>
2020-07-17 21:15:20 -0700

    users: better error handling

--- lib/user-crud/main.go
+++ lib/user-crud/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -28,7 +30,11 @@ type SpotifyUserInfoResponse struct {
 	Name       string `json:"display_name"`
 	ID         string `json:"id"`
 	ProfileUrl string `json:"href"`
-	Avatar     string `json:"images[0].url"`
+	Avatar     string `json:"images[0].url"` // TODO: this doesn't actually work
+}
+
+type ErrorResponseBody struct {
+	Message string `json:"message"`
 }
 
 type ResponseBody struct {
@@ -76,11 +82,17 @@ func coldstartInit() {
 		SPOTIFY_ACCOUNTS_BASE_URI = "https://accounts.spotify.com/api"
 		SPOTIFY_WEB_BASE_URI = "https://api.spotify.com/v1"
 	}
+
+	fmt.Println("Spotify Accounts URI: " + SPOTIFY_ACCOUNTS_BASE_URI)
+	fmt.Println("Spotify Web URI: " + SPOTIFY_WEB_BASE_URI)
+	if CLIENT_SECRET == "" {
+		panic("Client secret couldn't be retrieved")
+	}
 }
 
 // sends auth code to spotify token endpoint
 // gets access/refresh tokens back
-func getTokens(code string) SpotifyTokensResponse {
+func getTokens(code string) (SpotifyTokensResponse, error) {
 	response, err := http.PostForm(SPOTIFY_ACCOUNTS_BASE_URI+"/token", url.Values{
 		"grant_type":    {"authorization_code"},
 		"code":          {code},
@@ -94,15 +106,24 @@ func getTokens(code string) SpotifyTokensResponse {
 	tokenBody, err := ioutil.ReadAll(response.Body)
 	handleError(err)
 
-	// parse out access and refresh tokens
 	spotifyResponse := SpotifyTokensResponse{}
+	if response.StatusCode != 200 {
+		fmt.Println(string(tokenBody))
+		return spotifyResponse, errors.New("Didn't receive a 200 for the oauth exchange")
+	}
+
+	// parse out access and refresh tokens
 	err = json.Unmarshal([]byte(tokenBody), &spotifyResponse)
 	handleError(err)
 
-	return spotifyResponse
+	if spotifyResponse.AccessToken == "" || spotifyResponse.RefreshToken == "" {
+		return spotifyResponse, errors.New("Spotify didn't respond with a token")
+	}
+
+	return spotifyResponse, nil
 }
 
-func getUserInfo(token string) SpotifyUserInfoResponse {
+func getUserInfo(token string) (SpotifyUserInfoResponse, error) {
 	req, err := http.NewRequest("GET", SPOTIFY_WEB_BASE_URI+"/me", nil)
 	handleError(err)
 
@@ -114,29 +135,48 @@ func getUserInfo(token string) SpotifyUserInfoResponse {
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	handleError(err)
+	fmt.Println(string(body))
 
 	// parse out relevant user info
 	spotifyResponse := SpotifyUserInfoResponse{}
 	err = json.Unmarshal([]byte(body), &spotifyResponse)
 	handleError(err)
 
-	return spotifyResponse
+	return spotifyResponse, nil
+}
+
+func errorResponse(err error) events.APIGatewayProxyResponse {
+	// return info/credentials to client
+	response, err := json.Marshal(ErrorResponseBody{
+		Message: err.Error(),
+	})
+	handleError(err)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusBadRequest,
+		Body:       string(response),
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*", // TODO: not this
+		},
+	}
 }
 
 func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	if CLIENT_SECRET == "" {
-		coldstartInit()
-	}
-
 	// unmarshal json request into RequestBody object
 	requestBody := RequestBody{}
 	err := json.Unmarshal([]byte(req.Body), &requestBody)
 	handleError(err)
 
-	tokens := getTokens(requestBody.Code)
+	tokens, err := getTokens(requestBody.Code)
+	if err != nil {
+		return errorResponse(err), nil
+	}
 
 	// get user info from spotify
-	userInfo := getUserInfo(tokens.AccessToken)
+	userInfo, err := getUserInfo(tokens.AccessToken)
+	if err != nil {
+		return errorResponse(err), nil
+	}
 
 	// create/update user in DDB
 
@@ -145,6 +185,7 @@ func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 		AccessToken: tokens.AccessToken,
 		UserInfo:    userInfo,
 	})
+	handleError(err)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
@@ -156,5 +197,6 @@ func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 }
 
 func main() {
+	coldstartInit()
 	lambda.Start(handler)
 }
